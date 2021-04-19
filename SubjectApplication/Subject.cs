b7f0426bd439f4ca894709330c6d59616d193b14
stu-pksp117 <stu-pksp117@AMDVLA3511GW11>
2021-04-19 12:24:20 +0300

    Init Subject class

--- SubjectApplication/Subject.cs
+++ SubjectApplication/Subject.cs
@@ -6,5 +6,12 @@ namespace SubjectApplication
 {
     class Subject
     {
+
+        private string DisciplineName { get; set; }
+
+        private string TeacherLastName { get; set; }
+
+        private short Semester { get; set; }
+
     }
 }
