d98481fb6b50f8365ef61b91d83d753b91eae19b
stu-pksp117 <stu-pksp117@AMDVLA3511GW11>
2021-04-19 13:20:33 +0300

    added interaction with SubjectUtil

--- SubjectApplication/Program.cs
+++ SubjectApplication/Program.cs
@@ -7,7 +7,40 @@ namespace SubjectApplication
     {
         static void Main(string[] args)
         {
+            int length;
+            while (true)
+            {
+                Console.Write("Введите размер массива :: ");
+                if (int.TryParse(Console.ReadLine(), out length))
+                {
+                    break;
+                }
+                Console.Write("\t:: неверное значение, повтор...\n");
+            }
 
+            Console.Write("\tУспешно. Ввод массива...\n");
+            Subject[] plan = SubjectUtil.FillMassive(length);
+
+            Console.Write("\n\tУспешно. Сортировка...\n");
+            foreach (Subject subject in SubjectUtil.Sort(plan))
+            {
+                Console.WriteLine(string.Format("{0} {1} {2}", subject.DisciplineName, subject.TeacherLastName, subject.Semester));
+            }
+
+            Console.Write("\n\tУспешно. Сохранение в файл...\nВведите путь :: ");
+            string path = Console.ReadLine();
+            while (true)
+            {
+                try
+                {
+                    SubjectUtil.SaveIntoFile(plan, path);
+                    break;
+                }
+                catch (IOException)
+                {
+                    Console.Write("\t:: неверное значение, повтор...\n");
+                }
+            }
         }
 
     }
